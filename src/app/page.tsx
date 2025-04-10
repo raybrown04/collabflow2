2052c67b0edea50f882bfbbfce43cb26f261b250
Ray Brown <raybrown04@yahoo.com>
2025-04-10 20:36:49 +0000

    continue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,53 @@ import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '@/compo
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>CollabFlow Dashboard</CardTitle>
-          <CardDescription>Welcome to your unified workspace.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {/* Add dashboard widgets here */}
-          <p>Content will be placed here</p>
-        </CardContent>
-      </Card>
+    <div className="flex h-screen bg-gray-100">
+      <aside className="w-64 bg-gray-200 p-4">
+        <ul>
+          <li>
+            <a href="#" className="block py-2">
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block py-2">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block py-2">
+              Tasks
+            </a>
+          </li>
+          {/* Add more navigation items here */}
+        </ul>
+      </aside>
+      <main className="flex-1 p-4">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <Card>
+            <CardHeader>
+              <CardTitle>Widget 1</CardTitle>
+              <CardDescription>Description for Widget 1</CardDescription>
+            </CardHeader>
+            <CardContent>Content for Widget 1</CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Widget 2</CardTitle>
+              <CardDescription>Description for Widget 2</CardDescription>
+            </CardHeader>
+            <CardContent>Content for Widget 2</CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Widget 3</CardTitle>
+              <CardDescription>Description for Widget 3</CardDescription>
+            </CardHeader>
+            <CardContent>Content for Widget 3</CardContent>
+          </Card>
+          {/* Add more widgets here */}
+        </div>
+      </main>
     </div>
   );
 }
