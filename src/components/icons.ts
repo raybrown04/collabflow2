990c1740242e2e0a3c9756d72b770a3d75fb7aa9
Ray Brown <raybrown04@yahoo.com>
2025-04-10 20:51:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/components/icons.ts:35:14
    > Parsing ecmascript source code failed
    >   33 |     return (
    >   34 |       <>
    > > 35 |         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
    >      |              ^^^^^
    >   36 |           <path d="M3 8L12 2L21 8L12 14L3 8Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
    >   37 |           <path d="M3 16L12 10L21 16L12 22L3 16Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
    >   38 |         </svg>
    >
    > Expected '>', got 'width'
    >
    >

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -31,10 +31,12 @@ const Icons = {
   calendar: Calendar,
   dropbox: () => {
     return (
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 8L12 2L21 8L12 14L3 8Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M3 16L12 10L21 16L12 22L3 16Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      </svg>
+      <>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3 8L12 2L21 8L12 14L3 8Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M3 16L12 10L21 16L12 22L3 16Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </>
     );
   },
 };
